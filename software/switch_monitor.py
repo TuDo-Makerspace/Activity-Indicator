d7b2e1effabd5fb98e89fb9a62a6b83138532a11
Patrick Pedersen <ctx.xda@gmail.com>
2022-01-11 00:55:49 +0000

    switch_monitor: Check for active internet connection before monitoring switch
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- software/switch_monitor.py
+++ software/switch_monitor.py
@@ -2,25 +2,68 @@ import argparse
 import sys
 import RPi.GPIO as GPIO
 
+from enum import Enum
+from urllib.request import urlopen
+
+class con_led_state(Enum):
+        OFF = 0
+        RED = 1
+        GREEN = 2
+
+def check_connection():
+        try:
+                urlopen('http://www.google.com', timeout=1)
+                return True
+        except:
+                return False
+
+def set_con_led(red_pin: int, green_pin: int, state: con_led_state):
+        if state == con_led_state.OFF:
+                GPIO.output(red_pin, GPIO.LOW)
+                GPIO.output(green_pin, GPIO.LOW)
+        elif state == con_led_state.RED:
+                GPIO.output(red_pin, GPIO.HIGH)
+                GPIO.output(green_pin, GPIO.LOW)
+        elif state == con_led_state.GREEN:
+                GPIO.output(red_pin, GPIO.LOW)
+                GPIO.output(green_pin, GPIO.HIGH)
+
 # Parse arguments
 parser = argparse.ArgumentParser(description='Monitor and handle activity switch')
 parser.add_argument(
-        'GPIO',
+        'SWITCH_GPIO',
         type=int,
         help='GPIO connected to switch'
 )
+parser.add_argument(
+        'CON_LED_RED_GPIO',
+        type=int,
+        help='GPIO of red pin for connection LED'
+)
+parser.add_argument(
+        'CON_LED_GREEN_GPIO',
+        type=int,
+        help='GPIO of green pin for connection LED'
+)
 args = parser.parse_args()
 
-switch_pin = args.GPIO
+switch_pin = args.SWITCH_GPIO
+red_pin = args.CON_LED_RED_GPIO
+green_pin = args.CON_LED_GREEN_GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(red_pin, GPIO.OUT)
+GPIO.setup(green_pin, GPIO.OUT)
 
 prev_state = GPIO.input(switch_pin)
 
-sys.stdout.flush()
-
 while True:
+        while not check_connection():
+                set_con_led(red_pin, green_pin, con_led_state.RED)
+        
+        set_con_led(red_pin, green_pin, con_led_state.GREEN)
+
         curr_state = GPIO.input(switch_pin)
         if curr_state != prev_state:
                 if curr_state == GPIO.LOW:
