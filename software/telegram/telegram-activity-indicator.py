64eebbeec6c122221e064d671b57174bafcd2541
Patrick Pedersen <ctx.xda@gmail.com>
2022-01-11 03:47:39 +0000

    telegram: Parse log arg to logger
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- software/telegram/telegram-activity-indicator.py
+++ software/telegram/telegram-activity-indicator.py
@@ -41,7 +41,7 @@ parser.add_argument(
 args = parser.parse_args()
 
 # Start logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=args.log_level, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # Read config file
