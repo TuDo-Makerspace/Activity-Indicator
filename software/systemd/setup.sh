b21cd70b0c022e31ac15217bb7241f48fed1434d
Patrick Pedersen <ctx.xda@gmail.com>
2022-01-11 03:44:30 +0000

    systemd: Made service parameter-less
    Made switch monitor service parameter-less since we will handle GPIO info in a config file
    
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- software/systemd/setup.sh
+++ software/systemd/setup.sh
@@ -6,14 +6,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <Activity switch pin>"
-  exit 1
-fi
-
-systemctl stop switch_monitor@\*.service
+systemctl stop switch_monitor.service
 rm -rf /etc/systemd/system/switch_monitor*.service
-cp "$SCRIPT_DIR/switch_monitor@.service" /etc/systemd/system/
+cp "$SCRIPT_DIR/switch_monitor.service" /etc/systemd/system/
 systemctl daemon-reload
-systemctl enable switch_monitor@"$1".service
-systemctl start switch_monitor@"$1".service
+systemctl enable switch_monitor.service
+systemctl start switch_monitor.service
