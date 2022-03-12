1a3496c4530df73d401711ad90407a2134bd7127
CTXz <ctx.xda@gmail.com>
2022-03-12 02:59:07 +0100

    tools: ssh_sync: Added possibility to specify port
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- tools/ssh_sync.sh
+++ tools/ssh_sync.sh
@@ -9,17 +9,22 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT="$SCRIPT_DIR/.."
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <hostname>"
+  echo "Usage: $0 <hostname> [port]"
   exit 1
 fi
 
 HOSTNAME="$1"
+PORT=22
+
+if [ ! -z "$2" ]; then
+  PORT="$2"
+fi
 
 cd "$PROJECT_ROOT"
 PROJECT_DIR_NAME="$(basename "$PWD")"
 
 # Check if ssh connection can be established
-ssh -q -o BatchMode=yes "$HOSTNAME" exit
+ssh -p $PORT -q -o BatchMode=yes "$HOSTNAME" exit
 
 if [ $? != "0" ]; then
     echo "$0: Connection to remote device failed"
@@ -27,5 +32,5 @@ if [ $? != "0" ]; then
 fi
 
 # Remove remote project folder and push local project folder
-ssh "$HOSTNAME" rm -rf "\$HOME/$PROJECT_DIR_NAME"
-scp -r "$PWD" "$HOSTNAME":"\$HOME/"
+ssh -p $PORT "$HOSTNAME" rm -rf "\$HOME/$PROJECT_DIR_NAME"
+scp -P $PORT -r "$PWD" "$HOSTNAME":"\$HOME/"
